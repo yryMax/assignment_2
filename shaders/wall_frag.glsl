984b521133764d8c00b3d3c97e1e6386e08716e8
Yang Renyi <jianghurainxwy@gmail.com>
2024-11-01 02:58:26 +0100

    env mapping

--- shaders/wall_frag.glsl
+++ shaders/wall_frag.glsl
@@ -11,7 +11,10 @@ layout(std140) uniform Material // Must match the GPUMaterial defined in src/mes
 uniform sampler2D colorMap;
 uniform sampler2D normalMap;
 uniform int useNormalMapping;
-
+uniform int useTexture;
+uniform int useEnvMap;
+uniform samplerCube envMap;
+uniform vec3 cameraPos;
 
 in vec3 fragPosition;
 in vec3 fragNormal;
@@ -24,19 +27,33 @@ void main()
 {
     vec3 normal = normalize(fragNormal);
     vec2 texCoord = fragTexCoord; // Repeat texture
-    vec3 kd = texture(colorMap, texCoord).rgb;
+    vec3 basec = kd;
+    if (useTexture == 1)
+    {
+        basec = texture(colorMap, texCoord).rgb;
+    }
     vec3 normalMapColor = texture(normalMap, texCoord).rgb;
     if (useNormalMapping == 1)
     {
         normal = normalize(TBN * (2.0 * normalMapColor - 1.0));
     }
-    // phone shading
+
+
+    if (useEnvMap == 1)
+    {
+        vec3 I = normalize(fragPosition - cameraPos);
+        vec3 R = normalize(reflect(I, normal));
+        vec3 envColor = texture(envMap, R).rgb;
+        fragColor = vec4(envColor, 1);
+        return;
+    }
+
+    // phong shading
     vec3 lightDir = normalize(vec3(1, -1, 0));
     float diff = max(dot(normal, lightDir), 0.0);
-    vec3 diffuse = kd * diff;
-    vec3 ambient = kd * 0.6;
+    vec3 diffuse = basec * diff;
+    vec3 ambient = basec * 0.6;
     fragColor = vec4(diffuse + ambient, 1);
 
-   // else if (useMaterial)   { fragColor = vec4(kd, 1);
-    //else                    { fragColor = vec4(normal, 1);
+
 }
--- src/application.cpp
+++ src/application.cpp
@@ -1,6 +1,8 @@
 //#include "Image.h"
 #include "mesh.h"
 #include "texture.h"
+#include "CubeMapTexture.h"
+#include "framework/trackball.h"
 // Always include window first (because it includes glfw, which includes GL which needs to be included AFTER glew).
 // Can't wait for modules to fix this stuff...
 #include <framework/disable_all_warnings.h>
@@ -21,12 +23,23 @@ DISABLE_WARNINGS_POP()
 #include <iostream>
 #include <vector>
 
+std::vector<std::filesystem::path> faces = {
+    RESOURCE_ROOT "resources/environment_map/right.jpg",
+    RESOURCE_ROOT "resources/environment_map/left.jpg",
+    RESOURCE_ROOT "resources/environment_map/top3.jpg",
+    RESOURCE_ROOT "resources/environment_map/bottom5.jpg",
+    RESOURCE_ROOT "resources/environment_map/front.jpg",
+    RESOURCE_ROOT "resources/environment_map/back.jpg"
+};
+
 class Application {
 public:
     Application()
         : m_window("Final Project", glm::ivec2(1024, 1024), OpenGLVersion::GL41),
             m_wall_texture(RESOURCE_ROOT "resources/wall/texture.jpg"),
-            m_wall_normal(RESOURCE_ROOT "resources/wall/normal.jpg")
+            m_wall_normal(RESOURCE_ROOT "resources/wall/normal.jpg"),
+            m_env_map(faces),
+            m_trackball { &m_window, glm::radians(50.0f) }
     {
         m_window.registerKeyCallback([this](int key, int scancode, int action, int mods) {
             if (action == GLFW_PRESS)
@@ -42,7 +55,7 @@ public:
                 onMouseReleased(button, mods);
         });
 
-        m_wall = GPUMesh::loadMeshGPU(RESOURCE_ROOT "resources/wall/wall.obj");
+        m_wall = GPUMesh::loadMeshGPU(RESOURCE_ROOT "resources/wall/ball.obj");
 
         try {
 
@@ -54,6 +67,12 @@ public:
         } catch (ShaderLoadingException e) {
             std::cerr << e.what() << std::endl;
         }
+
+
+        glm::vec3 lookAt = glm::vec3(-0.215165, 0.305033, 0.645589);
+        glm::vec3 rotations = glm::vec3(0.450295, 0.617848, 0.0);
+        float dist = 3.0f;
+        m_trackball.setCamera(lookAt, rotations, dist);
     }
 
     void update()
@@ -67,6 +86,8 @@ public:
             // Use ImGui for easy input/output of ints, floats, strings, etc...
             ImGui::Begin("Window");
             ImGui::Checkbox("Normal Mapping", &m_useNormalMapping);
+            ImGui::Checkbox("Texture", &m_useTexture);
+            ImGui::Checkbox("Environment Map", &m_useEnvMap);
             ImGui::End();
 
             // Clear the screen
@@ -75,6 +96,9 @@ public:
 
             // ...
             glEnable(GL_DEPTH_TEST);
+            m_projectionMatrix = m_trackball.projectionMatrix();
+            m_viewMatrix = m_trackball.viewMatrix();
+
             const glm::mat4 mvpMatrix = m_projectionMatrix * m_viewMatrix * m_modelMatrix;
             // Normals should be transformed differently than positions (ignoring translations + dealing with scaling):
             // https://paroj.github.io/gltut/Illumination/Tut09%20Normal%20Transformation.html
@@ -92,6 +116,12 @@ public:
                 m_wall_normal.bind(GL_TEXTURE1);
                 glUniform1i(m_wallShader.getUniformLocation("normalMap"), 1);
                 glUniform1i(m_wallShader.getUniformLocation("useNormalMapping"), m_useNormalMapping ? GL_TRUE : GL_FALSE);
+                glUniform1i(m_wallShader.getUniformLocation("useTexture"), m_useTexture ? GL_TRUE : GL_FALSE);
+                glUniform1i(m_wallShader.getUniformLocation("useEnvMap"), m_useEnvMap ? GL_TRUE : GL_FALSE);
+                m_env_map.bind(GL_TEXTURE2);
+                glUniform1i(m_wallShader.getUniformLocation("envMap"), 2);
+                glad_glUniform3fv(m_wallShader.getUniformLocation("cameraPos"), 1, glm::value_ptr(m_trackball.position()));
+
 
                 mesh.draw(m_wallShader);
             }
@@ -144,8 +174,13 @@ private:
     Shader m_wallShader;
     std::vector<GPUMesh> m_wall;
     bool m_useNormalMapping = false;
+    bool m_useTexture = true;
+    bool m_useEnvMap = false;
     Texture m_wall_texture;
     Texture m_wall_normal;
+    CubeMapTexture m_env_map;
+    Trackball m_trackball;
+
 
 
     // Projection and view matrices for you to fill in and use
